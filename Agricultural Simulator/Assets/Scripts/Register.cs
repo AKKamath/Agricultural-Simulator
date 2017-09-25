54da65cecdfd62246333f3b0c392a19bb527e6e6
Aditya Kamath <a_kamath@hotmail.com>
2017-09-25 18:59:49 +0530

    Update login screen with moving birds

--- Agricultural Simulator/Assets/Scripts/Register.cs
+++ Agricultural Simulator/Assets/Scripts/Register.cs
@@ -2,31 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Text.RegularExpressions;
 
 public class Register : MonoBehaviour 
 {
-	private string secretKey = "farmersAreCool"; // Edit this value and make sure it's the same as the one stored on the server
-	public bool registering = false;
-	public string RegisterURL = "https://db-farmer.000webhostapp.com/create_account.php?"; //be sure to add a ? to your url
-	public Button yourButton;
+	// Secret key to communicate with server
+	private string secretKey = "farmersAreCool";
+	// URLs for PHP for accounts
+	private string registerURL = "https://db-farmer.000webhostapp.com/create_account.php?";
+	private string loginURL = "https://db-farmer.000webhostapp.com/login.php?";
+	// Buttons for registering/login
+	public Button registerButton;
+	public Button loginButton;
+	// Text Inputs
 	public InputField user;
 	public InputField pass;
+	// Popup on button click
 	public GameObject panel;
-	public Text panel_text;
-	public GameObject main;
+	public Text panelText;
+	public GameObject panelButton;
+	// Bird trigger
+	public Button bird;
+	// Used for debugging purposes
+	public string t;
 	void Start()
 	{
-		Button btn = yourButton;
-		btn.onClick.AddListener(Post);
+		registerButton.onClick.AddListener(registration);
+		loginButton.onClick.AddListener (login);
+		panelButton.GetComponent<Button>().onClick.AddListener (panelExit);
+		bird.onClick.AddListener (birdClick);
+		PlayerPrefs.SetInt ("Id", 0);
 	}
 	void Update()
 	{
-		if (registering) {
-			StartCoroutine (PostScores ());
-			//main.SetActive (false);
-			registering = false;
-		}
 	}
+	void birdClick()
+	{
+		panel.SetActive (true);
+		panelText.text = "In India 65% of the workforce is concentrated in agriculture. " +
+			"This brings in almost 14% of the GDP. However, farmers in the country remain" +
+			"dissatisfied. This is caused by various reason like fragmentation of land, lack of " +
+			"feasible loans and absence of infrastructure.\n\nTo learn more:\nhttps://qrius.com/the-bitter-plight-of-indias-small-farmers/";
+		panelButton.SetActive (true);
+	}
+	// Code for handling Md5 hash creation
 	public string Md5Sum(string strToEncrypt)
 	{
 		System.Text.UTF8Encoding ue = new System.Text.UTF8Encoding();
@@ -46,26 +66,61 @@ public class Register : MonoBehaviour
 
 		return hashString.PadLeft(32, '0');
 	}
-	void Post()
+	// Called when popup button is clicked
+	void panelExit()
 	{
-		registering = true;
+		// Reset popup
+		panelText.text = "Loading...";
+		panel.SetActive (false);
+		panelButton.SetActive (false);
+		// If successfully registered/logged in move to game
+		if (PlayerPrefs.GetInt ("Id") != 0)
+		{
+			SceneManager.LoadScene (1);
+		}
 	}
-	// remember to use StartCoroutine when calling this function!
-	IEnumerator PostScores()
+	// Called when register button clicked
+	void registration()
 	{
+		if (user.text == "" || pass.text == "") 
+		{
+			panel.SetActive (true);
+			panelText.text = "Enter username and password";
+			panelButton.SetActive (true);
+			return;
+		}
+		StartCoroutine (accountInput (registerURL));
+	}
+	// Called when login button clicked
+	void login()
+	{
+		if (user.text == "" || pass.text == "") 
+		{
+			panel.SetActive (true);
+			panelText.text = "Enter username and password";
+			panelButton.SetActive (true);
+			return;
+		}
+		StartCoroutine (accountInput (loginURL));
+	}
+	IEnumerator accountInput(string URL)
+	{
+		// Open up popup
 		panel.SetActive(true);
-		//This connects to a server side php script that will add the name and score to a MySQL DB.
-		// Supply it with a string representing the players name and the players score.
+		// Create verification hash
 		string hash = Md5Sum(user.text + pass.text + secretKey);
-
-		string post_url = RegisterURL + "User=" + WWW.EscapeURL(user.text) + "&hash=" + hash;
-
-		// Post the URL to the site and create a download object to get the result.
+		// URL to send data to
+		string post_url = URL + "User=" + WWW.EscapeURL(user.text) + "&hash=" + hash;
+		// Send password by POST
 		WWWForm form = new WWWForm();
 		form.AddField ("Pass", WWW.EscapeURL (pass.text));
 		WWW hs_post = new WWW(post_url, form);
-		yield return hs_post; // Wait until the download is done
-		panel.SetActive(true);
-		panel_text.text = hs_post.text;
+		yield return hs_post;
+		// Download complete, update popup
+		panelText.text = Regex.Replace(hs_post.text, "#.*", "");
+		if (Regex.Replace (hs_post.text, ".*#", "") != "" && Regex.Replace (hs_post.text, ".*#", "") != hs_post.text)
+			PlayerPrefs.SetInt ("Id", int.Parse (Regex.Replace (hs_post.text, ".*#", "")));
+		t = hs_post.text;
+		panelButton.SetActive (true);
 	}
 }
